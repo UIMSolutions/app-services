cf69d467130449091a129817661d1badb6e2ca7d
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-17 11:32:50 +0200

    Update

--- source/apps/services/package.d
+++ source/apps/services/package.d
@@ -2,13 +2,34 @@ module apps.services;
 
 mixin(ImportPhobos!());
 
-public { // required uim libraries
-  import uim.core;
-  import uim.oop;
-  import uim.apps;
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import uim.html;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+public import langs.javascript;
+
+public {
+  import apps.srv.controllers;
+  import apps.srv.helpers;
+  import apps.srv.routers;
+  import apps.srv.tests;
+  import apps.srv.views;
 }
 
-public { // admin-services libraries
-  import apps.services.controllers;
-  import apps.services.views;
+DApp servicesApp;
+static this() {
+  servicesApp = App
+    .name("servicesApp")
+    .rootPath("/apps/services")
+    .addRoute(Route("", HTTPMethod.GET, SRVIndexPageController))
+    .addRoute(Route("/", HTTPMethod.GET, SRVIndexPageController));
 }
